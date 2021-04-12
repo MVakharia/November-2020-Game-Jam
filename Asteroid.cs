126bc284fd08f470aba9384be2e3a4c1f41bf0ec
MVakharia <michaelvakharia@gmail.com>
2021-04-12 15:51:51 +0100

    Update Asteroid.cs

--- Asteroid.cs
+++ Asteroid.cs
@@ -1,81 +1,88 @@
 ﻿using UnityEngine;
 
-public enum Element
-{
-    Normal, Shock, Cryo
-}
+public enum Element { Normal, Shock, Cryo }
 
 public class Asteroid : MonoBehaviour
 {
-    private float movementSpeed = 100;
-
+    #region Fields
     [SerializeField]
     private GameObject metalCube;
-
     [SerializeField]
     private Element asteroidElement;
+    private AsteroidMesh thisMesh;
+    private Renderer thisRenderer;
+    #endregion
 
-    public float MovementSpeed { get => movementSpeed; private set => movementSpeed = value; }
-    public GameObject MetalCube { get => metalCube; private set => metalCube = value; }
-    public AsteroidMesh ThisMesh { get; private set; }
-    public Renderer ThisRenderer { get; private set; }
-    public Element AsteroidElement { get => asteroidElement; private set => asteroidElement = value; }
-    public Color ShockColor { get; private set; }
-    public Color CryoColor { get; private set; }
-    public int ShockLength { get; private set; }
-    public int FreezeLength { get; private set; }
+    #region Properties
+    public Element AsteroidElement => asteroidElement;
+    public float MovementSpeed => 100;
+    public Color ShockColor => new(0, .25F, 1);
+    public Color CryoColor => new(0, 1, 1);
+    public int ShockLength => 6;
+    public int FreezeLength => 6;
 
-    public void Start()
+    public AsteroidMesh ThisMesh
     {
-        ThisMesh = transform.GetChild(0).GetComponent<AsteroidMesh>();
-        ThisRenderer = transform.GetChild(0).GetComponent<Renderer>();
-
-        ShockColor = new Color(0, .25F, 1);
-        CryoColor = new Color(0, 1, 1);
-        ShockLength = 6;
-        FreezeLength = 6;
-
-        AsteroidElement = AssignedElement();
-
-        ThisRenderer.material.color = NewAsteroidColor();
+        get
+        {
+            if (thisMesh == null)
+            {
+                thisMesh = transform.GetChild(0).GetComponent<AsteroidMesh>();
+            }
+            return thisMesh;
+        }
     }
 
-    private void Update()
+    public Renderer ThisRenderer
     {
-        transform.Translate(0, 0, -MovementSpeed * Time.deltaTime);
+        get
+        {
+            if (thisRenderer == null)
+            {
+                thisRenderer = transform.GetChild(0).GetComponent<Renderer>();
+            }
+            return thisRenderer;
+        }
     }
+    #endregion
 
-    public void DestroyAsteroid ()
+    #region Methods
+    private void Move() => transform.Translate(0, 0, -MovementSpeed * Time.deltaTime);
+    public void DestroyAsteroid()
     {
         AsteroidSetup.Singleton.IncrementAsteroidsDestroyed();
         Destroy(gameObject);
     }
-
-    public void SpawnMetal ()
+    public void SpawnMetal() => Instantiate(metalCube, transform.position, transform.rotation);
+    private Element AssignedElement()
     {
-        Instantiate(MetalCube, transform.position, transform.rotation);
-    }
-
-    private Element AssignedElement ()
-    {
-        switch(Random.Range(0, 7))
+        switch (Random.Range(0, 7))
         {
             case 5: return Element.Shock;
             case 6: return Element.Cryo;
             default: return Element.Normal;
         }
     }
-
-    private Color NewAsteroidColor ()
+    private Color NewAsteroidColor()
     {
-        if(AsteroidElement == Element.Cryo)
+        if (asteroidElement == Element.Cryo)
         {
             return CryoColor;
         }
-        else if (AsteroidElement == Element.Shock)
+        else if (asteroidElement == Element.Shock)
         {
             return ShockColor;
         }
         return new Color(1, 1, 1);
     }
+    #endregion
+
+    public void Start()
+    {
+        asteroidElement = AssignedElement();
+
+        ThisRenderer.material.color = NewAsteroidColor();
+    }
+
+    private void Update() => Move();
 }
